0dadd92a397080c63c41666993a30052b25588c6
Umida <islamovaumida12@gmail.com>
2022-06-06 09:04:46 -0400

    second commit product,category

--- apps/category/views.py
+++ apps/category/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAdminUser
+from apps.category.models import Category
+from apps.category.permissions import IsAdminOrAllowAny
+from apps.category.serializers import CategorySerializer
 
-# Create your views here.
+
+class ListCreateCategoryView(generics.ListCreateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = (IsAdminOrAllowAny,)
+
